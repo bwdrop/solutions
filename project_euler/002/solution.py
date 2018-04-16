a91629b529cea3be35790b91f26de41c4b36e988
Heliane Ly <heliane.ly@epitech.eu>
2018-04-17 00:03:46 +0200

    Add project euler #2 in Python

---
+++ project_euler/002/solution.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from functools import reduce
+
+fib_list = [1, 2];
+
+def generate_fib_list():
+    fib = 0;
+    while fib <= 4000000:
+        i = len(fib_list);
+        fib = fib_list[i - 1] + fib_list[i - 2];
+        fib_list.append(fib);
+
+generate_fib_list();
+
+print(reduce((lambda x, y: x + y), filter(lambda x: x % 2 == 0, fib_list)));
