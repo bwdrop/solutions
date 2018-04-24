039bb1fd9567b0795e645c6314c67b01c985d601
Heliane Ly <heliane.ly@epitech.eu>
2018-04-24 17:52:24 +0200

    Add project euler #7 in Python

---
+++ project_euler/007/solution.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def check_if_prime(nb, primes):
+    for p in primes:
+        if nb % p == 0:
+            return False;
+    return True;
+
+def get_nth_prime(n):
+    primes = [2];
+    p = 3;
+    while len(primes) < n:
+        if check_if_prime(p, primes):
+            primes.append(p);
+        p += 2;
+    return primes.pop();
+
+print(get_nth_prime(10001));
