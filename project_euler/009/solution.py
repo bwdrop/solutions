a9916adf10e642a2db0ff942106018e12b876b56
Heliane Ly <heliane.ly@epitech.eu>
2018-04-26 15:21:43 +0200

    Add project euler #9 in Python

---
+++ project_euler/009/solution.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for a in range(1, 1001):
+    for b in range(a, 1001):
+        for c in range(b, 1001):
+            if a + b + c == 1000 and c**2 == a**2 + b**2:
+                print(a * b * c);
